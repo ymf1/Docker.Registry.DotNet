3e87bb2069996858deacca27dc308dc26fdc6644
Jaben Cargman <jaben.cargman@captiveaire.com>
2024-08-13 00:42:12 -0400

    Nullability and code coverage... we'll see how it goes.

--- src/Docker.Registry.DotNet/Application/Authentication/ParsedAuthentication.cs
+++ src/Docker.Registry.DotNet/Application/Authentication/ParsedAuthentication.cs
@@ -15,11 +15,11 @@
 
 namespace Docker.Registry.DotNet.Application.Authentication;
 
-internal class ParsedAuthentication(string realm, string service, string scope)
+internal class ParsedAuthentication(string? realm, string? service, string? scope)
 {
-    public string Realm { get; } = realm;
+    public string? Realm { get; } = realm;
 
-    public string Service { get; } = service;
+    public string? Service { get; } = service;
 
-    public string Scope { get; } = scope;
+    public string? Scope { get; } = scope;
 }
